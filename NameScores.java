31e7776996a5641d0d2ac7593fc1c44afcd16a3e
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-01 19:13:34 -0500

    Cleaned functions; started alphabitizing

--- NameScores.java
+++ NameScores.java
@@ -2,13 +2,12 @@ import java.nio.charset.Charset;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
 
 public class NameScores {
     
-    ArrayList<String> nameList = new ArrayList<String>();
     private String testListOfNames = "";
     private String[] testArray;
+    private long nameScoreTotal = 0;
     
     public NameScores (String fileName) {
         Charset charset = Charset.forName("US-ASCII");
@@ -18,21 +17,29 @@ public class NameScores {
             String line = null;
             while ((line = reader.readLine()) != null) {
                 testListOfNames += line;
-            }
-            
+            }    
         }
         catch (IOException x) {
             System.err.format("IOException: %s%n", x);
         }
         setNameList();
+        alphabitizeList();
     }   
     
     private void setNameList() {
-        testArray = new String[(testListOfNames.split("\",\"")).length];
-        testArray = testListOfNames.split("\",\"");
+        testArray = testListOfNames.split(",");
     }
     
     public String getNameList() {
-        return testArray[1];
+        return testArray[0] + " " + testArray[1];
+    }
+    
+    private void alphabitizeList() {
+        //toCharArray will be helpful for alphabitization
+        for(int x: testArray) {    
+            for(int i : testArray) {
+            
+            }
+        }
     }
 }
